bcb2a27edc0f3e61f0b2fab9727725e9be39c4a4
Arek Torczuk <torczuk@gmail.com>
2018-11-01 00:44:00 +0800

    Problem 57

--- BinaryTrees.hs
+++ BinaryTrees.hs
@@ -1,4 +1,4 @@
-module Trees
+module BinaryTrees
 (
   -- cbalTree,
   insert,
--- BinaryTreesTest.hs
+++ BinaryTreesTest.hs
@@ -1,24 +1,6 @@
 import Test.HUnit
-import Trees
+import BinaryTrees
 
-data Tree a = Empty | Branch a (Tree a) (Tree a) deriving (Show, Eq)
+testConstruct = TestCase (assertEqual "construct binary tree" (Branch 4 (Branch 2 Empty Empty) (Branch 10 Empty Empty)) (construct [4,2,10]))
 
-instance Functor Tree where
-  fmap f Empty = Empty
-  fmap f Branch n left right = Branch f n (fmap left) (fmap right)
-
---55. Construct completely balanced binary trees
-size :: Tree -> Int
-size Empty = 0
-size Branch left right = 1 + (size left) + (size right)
-
-insert :: Tree n -> n -> Tree n
-insert Empty e = Branch 1 Empty Empty
-insert Branch i left@(Branch _ _ _) right@Empty = Branch (i + 1) left (insert right e)
-insert Branch i left@Empty right@(Branch _ _ _) = Branch (i + 1) (insert left e) right
-insert Branch i left@(Branch m _ _) right@(Branch m _ _) e
-              | m > n = Branch (i + 1) left (insert right e)
-              | m <= n = Branch (i + 1) (insert left e) right
-
-cbalTree :: Int -> Tree
-cbalTree n = map (\t -> 'x') . foldr (\t n -> insert t n ) Empty $ [1 .. n]
+tests = TestList [TestLabel "construct" testConstruct]
